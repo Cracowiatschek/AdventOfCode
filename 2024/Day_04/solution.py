7d0e5d514bb397d38fe85dad1b3693eb494a9486
Cracowiatschek <piotrbaran0123@gmail.com>
2024-12-04 18:30:50 +0100

    Day 4 part two done

--- 2024/Day_04/solution.py
+++ 2024/Day_04/solution.py
@@ -1,55 +1,67 @@
 
 name: str = "Day 4: Ceres Search"
 
-
-def init_data(path):  # function for create input/test dataset
-    return open(path, 'r').read().split("\n")
-
-
-
-puzzle = init_data("input.in")
-
 def crossword_puzzle(puzzle: list[str]) -> int:
     xmas_count: int = 0
+
+    directions: dict[int,dict[str,list[int]]] = {
+        0: {"row": [0, 1, 2, 3], "column": [0, 0, 0, 0]},
+        1: {"row": [0, 0, 0, 0], "column": [0, 1, 2, 3]},
+        2: {"row": [0, 1, 2, 3], "column": [0, 1, 2, 3]},
+        3: {"row": [0, 1, 2, 3], "column": [3, 2, 1, 0]}
+    }
+
     for row_id, row_val in enumerate(puzzle):
         for col_id, col_val in enumerate(row_val):
             string_to_check: list[str] = []
 
-            try:
-                string_to_check.append("".join([row_val[col_id], row_val[col_id+1], row_val[col_id+2], row_val[col_id+3]]))
-            except Exception as e:
-                e = e
-
-            try:
-                string_to_check.append("".join([row_val[col_id], puzzle[row_id+1][col_id+1], puzzle[row_id+2][col_id+2], puzzle[row_id+3][col_id+3]]))
-            except Exception as e:
-                e = e
-
-            try:
-                string_to_check.append("".join([row_val[col_id], puzzle[row_id+1][col_id+1], puzzle[row_id+2][col_id+2], puzzle[row_id+3][col_id+3]]))
-            except Exception as e:
-                e = e
-
-            try:
-                string_to_check.append("".join([puzzle[row_id+3][col_id], puzzle[row_id+2][col_id], puzzle[row_id+1][col_id], row_val[col_id]]))
-            except Exception as e:
-                e = e
+            for direct in directions:
+                try:
+                    string_to_check.append("".join([
+                        puzzle[row_id + directions[direct]["row"][0]][col_id + directions[direct]["column"][0]],
+                        puzzle[row_id + directions[direct]["row"][1]][col_id + directions[direct]["column"][1]],
+                        puzzle[row_id + directions[direct]["row"][2]][col_id + directions[direct]["column"][2]],
+                        puzzle[row_id + directions[direct]["row"][3]][col_id + directions[direct]["column"][3]]
+                    ]))
+                except Exception as e:
+                    e = e
 
             for string in string_to_check:
 
-                revers_string = string[::-1]
+                revers_string: str = string[::-1]
 
                 if "XMAS" in [string,revers_string]:
                     xmas_count += 1
-                    print([row_id,col_id],[string,revers_string], xmas_count)
 
     return xmas_count
 
+def find_x_mas(puzzle:list[str]) -> int:
+    x_mas_count: int = 0
+    find_x:list[str] = ["MMASS", "MSAMS"]
+    for row_id, row_val in enumerate(puzzle):
+        for col_id, col_val in enumerate(row_val):
+            string_to_check: str = ""
+            try:
+                string_to_check = "".join([
+                    puzzle[row_id][col_id],
+                    puzzle[row_id][col_id+2],
+                    puzzle[row_id+1][col_id+1],
+                    puzzle[row_id+2][col_id],
+                    puzzle[row_id+2][col_id+2]
+                ])
+            except Exception as e:
+                e=e
 
-print(crossword_puzzle(puzzle))
-# def output(data: list[str]) -> tuple:  # get solution outputs for framework
-#
-#     # solution body
-#
-#     return a, b  # change variables to correct
-#
+            for x in find_x:
+                if x in [string_to_check, string_to_check[::-1]]:
+                    x_mas_count += 1
+
+    return x_mas_count
+
+
+def output(data: list[str]) -> tuple:  # get solution outputs for framework
+
+    solution_1: int = crossword_puzzle(data)
+    solution_2: int = find_x_mas(data)
+
+    return solution_1, solution_2  # change variables to correct
--- 2024/Day_04/test.py
+++ 2024/Day_04/test.py
@@ -2,8 +2,19 @@
 import unittest
 from solution import output
 
+
+class TestCrossword(unittest.TestCase):
+    def test_part_one(self):
+        solution_1: int = output(data)[0]
+        self.assertEqual(solution_1, 18)
+
+    def test_part_two(self):
+        solution_2: int = output(data)[1]
+        self.assertEqual(solution_2, 9)
+
+
 def run_tests():  # runing test module for exec module
-    suite = unittest.TestLoader().loadTestsFromTestCase(xyz)  # change xyz to test case
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCrossword)  # change xyz to test case
     runner = unittest.TextTestRunner()
     result = runner.run(suite)
     simple = result.wasSuccessful()
