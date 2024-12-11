7c3d5898d966571ecf9351afb350590697310edf
Cracowiatschek <piotrbaran0123@gmail.com>
2024-12-11 11:09:24 +0100

    Day 11 part two,

--- 2024/Day_11/solution.py
+++ 2024/Day_11/solution.py
@@ -8,14 +8,19 @@ def init_data(path):  # function for create input/test dataset
 def read_data(data: list[str]) -> list[int]:
     return list(map(int,data[0].split(' ')))
 #
-# def check_stone(stone: int) -> int or list[int]:
-#     stone_str: str = str(stone)
-#     if stone == 0:
-#         return 1
-#     elif len(stone_str) > 1 and  len(stone_str) % 2 == 0:
-#         return int(stone_str[:len(stone_str)//2]), int(stone_str[len(stone_str)//2:])
-#     else:
-#         return stone * 2024
+def check_stone(stone: int) -> int or list[int]:
+
+    if stone == 0:
+        return [1]
+    stone_str: str = str(stone)
+    stone_len: int = len(stone_str)
+
+
+    if stone_len % 2 == 0:
+        stone_center: int = stone_len // 2
+        return [int(stone_str[:stone_center]), int(stone_str[stone_center:])]
+
+    return [stone * 2024]
 
 def blink(stones: list[int], blink_count: int) -> int:
     with tqdm(total = blink_count) as bar:
@@ -23,20 +28,17 @@ def blink(stones: list[int], blink_count: int) -> int:
             bar.update(1)
             stones_after_blink: list[int] = []
             for stone in stones:
-                stone_str: str = str(stone)
-                stone_len: int = len(stone_str)
-                if stone == 0:
-                    stones_after_blink.append(1)
-                elif len(stone_str) > 1 and len(stone_str) % 2 == 0:
-                    stones_after_blink.append(int(stone_str[:stone_len // 2]))
-                    stones_after_blink.append(int(stone_str[stone_len // 2:]))
-                else:
-                    stones_after_blink.append(stone * 2024)
-            stones = stones_after_blink.copy()
+                stones_after_blink += check_stone(stone)
+                # if type(new_stone) is tuple:
+                #     stones_after_blink.append(new_stone[0])
+                #     stones_after_blink.append(new_stone[1])
+                # else:
+                #     stones_after_blink.append(new_stone)
+            stones = stones_after_blink
     return len(stones)
 
 print(blink(read_data(init_data("input.in")), 25))
-# print(blink(read_data(init_data("input.in")), 75))
+print(blink(read_data(init_data("input.in")), 75))
 
 def output(data: list[str]) -> tuple:  # get solution outputs for framework
     # solution body
