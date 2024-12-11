2c5e289c9ac51ab9198982e830cf83c506c8e70b
Cracowiatschek <piotrbaran0123@gmail.com>
2024-12-11 15:27:30 +0100

    Day 11 part two, done

--- 2024/Day_11/solution.py
+++ 2024/Day_11/solution.py
@@ -1,4 +1,5 @@
 from tqdm import tqdm
+from collections import Counter
 
 name: str = "Day 11: Plutonian Pebbles"
 
@@ -9,7 +10,6 @@ def read_data(data: list[str]) -> list[int]:
     return list(map(int,data[0].split(' ')))
 #
 def check_stone(stone: int) -> int or list[int]:
-
     if stone == 0:
         return [1]
     stone_str: str = str(stone)
@@ -23,27 +23,34 @@ def check_stone(stone: int) -> int or list[int]:
     return [stone * 2024]
 
 def blink(stones: list[int], blink_count: int) -> int:
-    with tqdm(total = blink_count) as bar:
-        for _ in range(blink_count):
-            bar.update(1)
-            stones_after_blink: list[int] = []
-            for stone in stones:
-                stones_after_blink += check_stone(stone)
-                # if type(new_stone) is tuple:
-                #     stones_after_blink.append(new_stone[0])
-                #     stones_after_blink.append(new_stone[1])
-                # else:
-                #     stones_after_blink.append(new_stone)
-            stones = stones_after_blink
-    return len(stones)
+    cache: dict[int:int] = {}
+    with tqdm(total = blink_count*len(stones)) as bar:
+        for stone in stones:
+            stone_cache: dict = {stone: 1}
+            for _ in range(blink_count):
+                bar.update(1)
+                stones_after_blink: dict[int:int] = {}
+                for cached_stone in stone_cache:
+                    for i in check_stone(cached_stone):
+                        if i in stones_after_blink:
+                            stones_after_blink[i] += stone_cache[cached_stone]
+                        else:
+                            stones_after_blink[i] = stone_cache[cached_stone]
+                stone_cache = stones_after_blink
 
-print(blink(read_data(init_data("input.in")), 25))
-print(blink(read_data(init_data("input.in")), 75))
+            for cached_stone in stone_cache:
+                if cached_stone in cache:
+                    cache[cached_stone] += stone_cache[cached_stone]
+                else:
+                    cache[cached_stone] = stone_cache[cached_stone]
+    result = [cache[i] for i in cache]
+    return sum(result)
+
 
 def output(data: list[str]) -> tuple:  # get solution outputs for framework
     # solution body
-    solution_1: int = 0
-    solution_2: int = 0
+    solution_1: int = blink(read_data(data), 25)
+    solution_2: int = blink(read_data(data), 75)
 
     return solution_1, solution_2  # change variables to correct
                 
--- 2024/Day_11/test.py
+++ 2024/Day_11/test.py
@@ -2,8 +2,12 @@
 import unittest
 from solution import output
 
+class TestPlutonianPebbles(unittest.TestCase):
+    def test_output(self):
+        self.assertEqual(output(data)[0], 55312)
+
 def run_tests():  # runing test module for exec module
-    suite = unittest.TestLoader().loadTestsFromTestCase(xyz)  # change xyz to test case
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestPlutonianPebbles)  # change xyz to test case
     runner = unittest.TextTestRunner()
     result = runner.run(suite)
     simple = result.wasSuccessful()
