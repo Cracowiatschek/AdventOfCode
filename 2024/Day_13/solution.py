9e907f837b46ba493d1656eacb470d29ea87ceac
Cracowiatschek <piotrbaran0123@gmail.com>
2025-01-06 23:08:04 +0100

    Day 13 part two, done

--- 2024/Day_13/solution.py
+++ 2024/Day_13/solution.py
@@ -2,6 +2,9 @@
 from collections import namedtuple
 from dataclasses import dataclass
 
+import sympy.core.numbers
+from sympy import symbols, Eq, solve
+
 name: str = "Day 13: Claw Contraption"
 
 dt: list[str] = open("input.in", 'r').read().split("\n")
@@ -46,38 +49,27 @@ def read_data(data: list[str]) -> list[Machine]:
 
 machines = read_data(dt)
 
-def cost_calculator(machines_set, correct_factor: int):
+
+def cost_solver(machines_set, correct_factor: int):
     result: int = 0
     for game in machines_set:
-        cost: list[int] = []
         a: Button = game.buttons[0]
         b: Button = game.buttons[1]
         prize: Prize = Prize(X = game.prize.X + correct_factor, Y = game.prize.Y + correct_factor)
-        push_a: int = -1
+        x, y = symbols("x y")
 
-        while push_a < 100:
-            push_a += 1
-            x: int = 0
-            y: int = 0
-            x += a.X * push_a
-            y += a.Y * push_a
+        eq1 = Eq(a.X * x + b.X * y, prize.X)
+        eq2 = Eq(a.Y * x + b.Y * y, prize.Y)
+        solution = solve((eq1, eq2), (x, y))
+        press_a = solution.get(list(solution.keys())[0])
+        press_b = solution.get(list(solution.keys())[1])
 
-            b_x: int = (prize.X - x) // b.X
-            b_y: int = (prize.Y - y) // b.Y
-
-            mod_x: int = (prize.X-x) % b.X
-            mod_y: int = (prize.Y-y) % b.Y
-
-            if b_x == b_y and mod_y + mod_x == 0:
-                cost.append(push_a * a.Cost + b_x * b.Cost)
-
-        if len(cost) > 0:
-            result += min(cost)
+        if type(press_a) is sympy.core.numbers.Integer and type(press_b) is sympy.core.numbers.Integer:
+            result += press_a * a.Cost + press_b * b.Cost
     return result
 
-print(cost_calculator(machines, 0))
-
-print(cost_calculator(machines, 10000000000000))
+print(cost_solver(machines, 0))
+print(cost_solver(machines, 10000000000000))
 
 
 
